42fcfc7744a9c37b6d1571793d86782d588a6040
carolinamabad <158006775+carolinamabad@users.noreply.github.com>
2025-10-02 20:14:20 -0400

    fixed the averages calculations and added the wrapper

--- grade-calculator/grade_calculator.go
+++ grade-calculator/grade_calculator.go
@@ -38,6 +38,10 @@ func NewGradeCalculator() *GradeCalculator {
 	}
 }
 
+func (gc *GradeCalculator) GetGrade() string {
+	return gc.GetFinalGrade()
+}
+
 func (gc *GradeCalculator) GetFinalGrade() string {
 	numericalGrade := gc.calculateNumericalGrade()
 
@@ -80,7 +84,7 @@ func (gc *GradeCalculator) AddGrade(name string, grade int, gradeType GradeType)
 func (gc *GradeCalculator) calculateNumericalGrade() int {
 	assignment_average := computeAverage(gc.assignments)
 	exam_average := computeAverage(gc.exams)
-	essay_average := computeAverage(gc.exams)
+	essay_average := computeAverage(gc.essays)
 
 	weighted_grade := float64(assignment_average)*.5 + float64(exam_average)*.35 + float64(essay_average)*.15
 
@@ -88,10 +92,13 @@ func (gc *GradeCalculator) calculateNumericalGrade() int {
 }
 
 func computeAverage(grades []Grade) int {
-	sum := 0
+	if len(grades) == 0 {
+		return 0
+	}
+	sum:= 0
 
-	for grade, _ := range grades {
-		sum += grade
+	for _, g := range grades {
+		sum += g.Grade
 	}
 
 	return sum / len(grades)
